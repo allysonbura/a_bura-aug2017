dd9c9bd8d86924c839d8ce3605c2219ea50d52d1
allyson bura <allyson.bura@gmail.com>
2017-08-22 09:28:06 -0700

    second project -- using Git from Eclipse

---
+++ project2/src/project2.java
@@ -0,0 +1,14 @@
+
+// allyson bura
+//My second java project. It will compare print and printIn.
+//August 22, 2017
+
+public class project2 {
+
+	public static void main(String[] args) {
+		System.out.println("hello -- this is project 2!!");
+		
+		System.out.print("hello -- this is project 2!!");
+	}
+
+}
